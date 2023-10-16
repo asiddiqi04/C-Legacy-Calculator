d56b173b4a3eb8dd8ec1f31b08f190bb85151b1f
Adil Siddiqi <asiddi18@linux-01.socs.uoguelph.ca>
2023-10-16 11:10:00 -0400

    Added hex, octal, and binary addition and subtraction

--- cis3250_legacy_calculator_files2/addsubtract.c
+++ cis3250_legacy_calculator_files2/addsubtract.c
@@ -67,30 +67,26 @@ int binarySubtraction(int binary1, int binary2) {
 
 // Function to add two octal numbers
 int octalAddition(int octal1, int octal2) {
-    int carry = 0, result = 0, placeValue = 1;
+    // Returns the sum of two octal numbers
+    return octal1 + octal2;
+}
 
-    while (octal1 > 0 || octal2 > 0 || carry > 0) {
-        int digit1 = octal1 % 10;
-        int digit2 = octal2 % 10;
 
-        // Add the digits along with the carry
-        int sum = digit1 + digit2 + carry;
+int octalSubtract(int octal1, int octal2) {
+    //Returns the difference of two octal numbers 
+    return octal1 - octal2;
+}
 
-        // Calculate the carry for the next iteration
-        carry = sum / 8;
+int hexAdd(int hex1, int hex2)
+{
+    // Returns the sum of two hex numbers 
+    return hex1 + hex2;
+}
 
-        // Update the result with the sum at the current place value
-        result += (sum % 8) * placeValue;
-
-        // Move to the next place value
-        placeValue *= 10;
-
-        // Remove the last digits from octal1 and octal2
-        octal1 /= 10;
-        octal2 /= 10;
-    }
-
-    return result;
+int hexSubtract(int hex1, int hex2)
+{   
+    // returns the difference of two hex numbers
+    return hex1 - hex2;
 }
 
 
@@ -101,6 +97,7 @@ int octalAddition(int octal1, int octal2) {
 int main() {
     int binary1, binary2;
     int octal1, octal2;
+    int hex1, hex2;
 
     int choice;
     int operation;
@@ -128,9 +125,9 @@ int main() {
             printf("Enter the second binary number: ");
             scanf("%d", &binary2);
 
-            int sum = binaryAddition(binary1, binary2);
-
-            printf("Sum of the two binary numbers: %d\n", sum);
+            //int sum = binaryAddition(binary1, binary2);
+            int sum = binary1 + binary2;
+            printf("Sum of the two binary numbers: 0b%d\n", sum);
 
 
         }
@@ -163,17 +160,64 @@ int main() {
         if(operation == 1)
         {
             printf("Enter the first octal number: ");
-            scanf("%d", &octal1);
+            scanf("%o", &octal1);
 
             printf("Enter the second octal number: ");
-            scanf("%d", &octal2);
+            scanf("%o", &octal2);
 
             int sum = octalAddition(octal1, octal2);
-
-            printf("Sum of the two octal numbers: %d\n", sum);
+            
+            printf("Sum of the two octal numbers: %o\n", sum);
 
 
         }
+        else if(operation == 2)
+        {
+            printf("Enter the first octal number: ");
+            scanf("%o", &octal1);
+
+            printf("Enter the second octal number: ");
+            scanf("%o", &octal2);
+
+            int difference = octalSubtract(octal1, octal2);
+            printf("Difference of the two octal numbers: %o\n",difference);
+        }
+    }
+
+    if(choice == 3)
+    {
+        printf("\n1. Add");
+        printf("\n2. Subtract");
+
+        printf("\nEnter operation: ");
+        scanf("%d", &operation);
+
+        if(operation == 1)
+        {
+            printf("Enter the first hex number: ");
+            scanf("%x", &hex1);
+
+            printf("Enter the second hex number: ");
+            scanf("%x", &hex2);
+
+            int sum = hexAdd(hex1, hex2);
+            
+            printf("Sum of the two hex numbers: %x\n", sum);
+
+
+        }
+        else if(operation == 2)
+        {
+            printf("Enter the first octal number: ");
+            scanf("%x", &hex1);
+
+            printf("Enter the second octal number: ");
+            scanf("%x", &hex2);
+
+            int difference = hexSubtract(octal1, octal2);
+            printf("Difference of the two hex numbers: %x\n",difference);
+        }
+
     }
 
 }

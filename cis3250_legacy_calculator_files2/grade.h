06dcb1c87da3c27f8b4eb2c161958b54341f15cf
Tanveer Sadat <tsadat@linux-08.socs.uoguelph.ca>
2023-10-23 05:34:46 -0400

    added comments

--- cis3250_legacy_calculator_files2/grade.h
+++ cis3250_legacy_calculator_files2/grade.h
@@ -1,15 +1,32 @@
+/*
+  Name: Tanveer Sadat and Benjamin Gobraeil
+  Program Description:
+  This is the gradeCalculator function. It takes four parameters: 
+  numAssessments (the number of assessments), grades (an array of grades), weights (an array of weights), 
+  and dropLowest (a flag indicating whether to drop the lowest grade).
+
+*/
+
 #include <stdio.h>
 
 void gradeCalculator(int numAssessments, float grades [], float weights [], int dropLowest){
     			
     float grade = 0.0, weight = 0.0, lowest = grades[0], lowestWeight;
 
+    printf("Report of Grades and Weights:\n");
+
     for (int a = 0; a < numAssessments; a++){
+        // Make sure grades and weights are positive values
         if (grades[a] >= 0 && weights[a] >= 0){
+
+            // Keep track of the lowest grade and weight
             if (grades[a] < lowest){
                 lowest = grades[a];
                 lowestWeight = weights[a];
             }
+
+            printf("Assessment %d: Grade = %.2f, Weight = %.2f\n", a + 1, grades[a], weights[a]);
+
             grade = grade + (grades[a] * weights[a]);
             weight = weight + weights[a];
         }
@@ -28,7 +45,9 @@ void gradeCalculator(int numAssessments, float grades [], float weights [], int 
         return;
     }
 
+    // Drop the lowest grade when asked to
     if (dropLowest == 1){
+        printf("Dropped Lowest Grade: %.2f, Weight: %.2f\n", lowest, lowestWeight);
         grade = grade - (lowest * lowestWeight);
         weight = weight - lowestWeight;
     }
